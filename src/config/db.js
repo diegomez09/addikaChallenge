e26ec90d8ed96fc0e9057a90776563b0ced210b3
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-05 11:16:35 -0600

    Test db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
-
+//Initialize connection
 const sequelize = new Sequelize('sql5396912','sql5396912','rFdzPKkb3N',{
     host:'sql5.freemysqlhosting.net:3306',
     dialect:'mysql'
 });
+//Test connection
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
