afe3b70916e99857e308a175707390e9bdc38cc6
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-05 11:11:38 -0600

    Db Connection File

---
+++ src/config/db.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('sql5396912','sql5396912','rFdzPKkb3N',{
+    host:'sql5.freemysqlhosting.net:3306',
+    dialect:'mysql'
+});
