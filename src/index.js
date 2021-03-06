eeb5a64e3702e194f65ca907b205c3e43b57c0a7
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 12:09:28 -0600

    Index: Body parser correciton

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@
 // Requires
 // ===========================
 const express = require('express');
+require('./config/db');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/user')
 // ===========================
 // Initializations
 // ===========================
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(userRouter);
 // ===========================
 // Middleware
 // ===========================
@@ -13,8 +21,8 @@ app.use(express.json());
 // ===========================
 // Port
 // ===========================
-app.set('port',process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000)
 
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log('Listening port: ', app.get('port'));
 })
