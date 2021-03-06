beb7e9eb5afa9e25156fa897a3fbb262f1a10f35
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 17:23:31 -0600

    User: CRUD works

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,23 +6,26 @@ const router = new express.Router();
 const bcryptjs = require('bcryptjs');
 const { verifyToken } = require("../middleware/auth");
 const { User } = require('../config/db');
-const { check, calidationResult, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
+const { verifyRole } = require('../middleware/role');
 // ===========================
 // Initializations
 // ===========================
 
 /**
-  * @api {POST} /user?token={token} Create new user
+  * @api {POST} /user?token={token}&role={role} Create new user
   * @apiGroup User
   * @apiDescription This method adds a new user in the database
   * @apiVersion  1.0.0
   * @apiParam  {String} token JWT
+  * @apiParam  {String} role string
   * 
   *  @apiParamExample  {type} Request-Example:
    {
    "role":"admin",
    "email":"admin@mail.com",
-   "password":"123456"
+   "password":"123456",
+   "permissions":"ALL"
     }
   *
   * @apiSuccessExample {type} Success-Response:
@@ -32,16 +35,17 @@ const { check, calidationResult, validationResult } = require('express-validator
         "role": "admin",
         "email": "admin@mail.com",
         "password": ":D",
-        "permissions":true
+        "permissions":"ALL"
     }
     }
   */
 router.post('/user', [
-    //verifyToken,
+    verifyToken,
+    verifyRole,
     check('role', 'Role is required').not().isEmpty(),
     check('email', 'Email is required').not().isEmpty(),
     check('password', 'Password is required').not().isEmpty(),
-    check('permissions','Permissions is required')
+    check('permissions', 'Permissions is required')
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(422).json({ error: errors.array() });
@@ -62,11 +66,12 @@ router.post('/user', [
 })
 
 /**
- * @api {GET} /user?token={token} Get All Users
+ * @api {GET} /user?token={token}&role={role} Get All Users
  * @apiGroup User
  * @apiDescription This method returns all the users registered in the data base
  * @apiVersion  1.0.0
  * @apiParam  {String} token JWT
+ * @apiParam  {String} role string
  * 
  * @apiSuccessExample {type} Success-Response:
  * {
@@ -85,9 +90,9 @@ router.post('/user', [
     }
  * 
  */
-router.get('/user', [verifyToken], async (req, res) => {
+router.get('/user', [verifyToken, verifyRole], async (req, res) => {
     const users = await User.findAll({
-        attributes:{
+        attributes: {
             exclude: ['password']
         }
     });
@@ -97,4 +102,84 @@ router.get('/user', [verifyToken], async (req, res) => {
         data: users
     });
 })
+
+/**
+  * @api {PUT} /user/{id}?token={token}&role={role} Update user
+  * @apiGroup User
+  * @apiDescription This method adds a new user in the database
+  * @apiVersion  1.0.0
+  * @apiParam  {String} id Id of the document to be updated
+  * @apiParam  {String} token JWT
+  * @apiParam  {String} role string
+  * 
+  *  @apiParamExample  {type} Request-Example:
+   {
+    "role":"admin",
+    "email":"admin@mail.com",
+    "permissions":"ALL"
+}
+  *
+  * @apiSuccessExample {type} Success-Response:
+  * {
+    "success": true,
+    "user": {
+        "id": 1,
+        "role": "admin",
+        "email": "admin@mail.com",
+        "permissions": "ALL",
+        "createdAt": "2021-03-06T20:21:18.000Z",
+        "updatedAt": "2021-03-06T23:09:10.000Z"
+    }
+}
+  */
+router.put("/user/:id", [verifyToken, verifyRole], async (req, res) => {
+    const user = await User.update(req.body, {
+        where: { id: req.params.id }
+    }).catch();
+    const userUpdate = await User.findByPk(req.params.id, {
+        where: { id: req.params.id },
+        attributes: { exclude: ['password'] }
+    });
+    return res.status(200).json({
+        success: true,
+        user: userUpdate,
+    });
+});
+
+/**
+   * @api {DELETE} /user/{id}?token={token}&role{role} Delete user
+   * @apiGroup User
+   * @apiDescription This method do a logical delete of the user from de database
+   * @apiVersion  1.0.0
+   * @apiParam  {String} id Id of the document to be deleted
+   * @apiParam  {String} token JWT
+   * @apiParam  {String} role string
+   * 
+   * @apiSuccessExample {type} Success-Response:
+   * {
+    "success": true,
+    "user": {
+        "id": 1,
+        "role": "ADMIN",
+        "email": "admin@mail.com",
+        "permissions": "ALL",
+        "createdAt": "2021-03-06T23:20:29.000Z",
+        "updatedAt": "2021-03-06T23:20:29.000Z"
+    }
+}
+   */
+router.delete("/user/:id", [verifyToken, verifyRole], async (req, res) => {
+    let id = req.params.id;
+    const userUpdate = await User.findByPk(req.params.id, {
+        where: { id: req.params.id },
+        attributes: { exclude: ['password'] }
+    });
+    await User.destroy({
+        where: { id: id }
+    })
+    return res.status(200).json({
+        success: true,
+        user:userUpdate
+    });
+});
 module.exports = router;
