da61f062d330b518e3424e3afc3b6a48201d0339
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 12:09:53 -0600

    User: Model

---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('user', {
+        id: {
+            type: type.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+            unique: true
+        },
+        role: {
+            type: type.STRING,
+            allowNull: false
+        },
+        email: {
+            type: type.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: type.STRING,
+            allowNull: false
+        }
+    })
+}
