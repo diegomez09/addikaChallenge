583bec607f11d5c977aa3542631997935bee6f20
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 14:17:11 -0600

    Post: Model created

---
+++ src/models/post.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('post', {
+        id: {
+            type: type.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+            unique: true
+        },
+        title: {
+            type: type.STRING,
+            allowNull: false
+        },
+        author: {
+            type: type.STRING,
+            allowNull: false,
+            unique: true
+        },
+        body: {
+            type: type.STRING,
+            allowNull: false
+        }
+    })
+}
