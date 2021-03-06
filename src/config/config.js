cc16797558d0350dea1a7749a3112ec8d57ae5f5
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 13:52:53 -0600

    Config file

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+// ===========================
+//  Port
+// ===========================
+module.exports.PORT = 3000;
+// ===========================
+// SEED JWT
+// ===========================
+module.exports.SEED = "4ddik4T3$T";
+// ===========================
+// TOKEN EXPIRATION TIME (seconds*minutes*hours)
+// ===========================
+module.exports.TOKEN_EXP_TIME = 60 * 60 * 3;
