5b2656bc4ba937d4bf3e7dbfd9213db6b7066fa1
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 12:09:44 -0600

    Db: Local host connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,31 @@
+// ===========================
+// Requires
+// ===========================
 const Sequelize = require('sequelize');
+// ===========================
+// Models
+// ===========================
+const UserModel = require('../models/user');
 //Initialize connection
-const sequelize = new Sequelize('sql5396912','sql5396912','rFdzPKkb3N',{
-    host:'sql5.freemysqlhosting.net:3306',
-    dialect:'mysql'
+const sequelize = new Sequelize('addika', 'user', 'rFdzPKkb3N', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 //Test connection
-try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+// try {
+//   sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
+
+const User = UserModel(sequelize, Sequelize);
+
+sequelize.sync({force:false})
+.then(()=>{
+  console.log('Tablas creadas');
+});
+
+module.exports={
+  User
+}
