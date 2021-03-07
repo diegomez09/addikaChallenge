b0cf1fd20d4c393f292914bb18807cd9d9a67585
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 22:29:14 -0600

    Role: Return json added

--- src/middleware/role.js
+++ src/middleware/role.js
@@ -5,14 +5,26 @@ const jwt = require('jsonwebtoken');
 
 let create = (req, res, next) => {
     if (req.user.permissions != 'UPDATE') next();
+    else res.status(403).json({
+        success: false,
+        message: "Invalid access"
+    });
 }
 
 let update = (req, res, next) => {
     if (req.user.permissions != 'CREATE') next();
+    else res.status(403).json({
+        success: false,
+        message: "Invalid access"
+    });
 }
 
 let adminRole = (req, res, next) => {
     if (req.user.permissions == 'ADMIN') next();
+    else res.status(403).json({
+        success: false,
+        message: "Invalid access"
+    });
 }
 module.exports = {
     create,
