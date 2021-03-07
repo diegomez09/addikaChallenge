63ebc42c4e71a8d13efec7c21c51bbbaa81b6a5b
Diego Gómez <diegofabriciogg@gmail.com>
2021-03-06 19:23:04 -0600

    Role: Middleware role jwt

--- src/middleware/role.js
+++ src/middleware/role.js
@@ -1,11 +1,40 @@
+const jwt = require('jsonwebtoken');
 // =========
 // Verificar role
 //==============
 
-let verifyRole = (req,res,next)=>{
+let create = (req, res, next) => {
+    let token = req.query.token;
+    let decoded = jwt.decode(token);
+    let permissions = decoded.user.permissions;
+    if (permissions == 'CREATE' || permissions == 'ADMIN') next();
+    else res.status(422).json({
+        succes: false,
+        error: 'Access denied'
+    })
+}
+
+let update = (req, res, next) => {
+    let token = req.query.token;
+    let decoded = jwt.decode(token);
+    let permissions = decoded.user.permissions;
+    if (permissions == 'UPDATE' || permissions == 'ADMIN') next();
+    else res.status(422).json({
+        succes: false,
+        error: 'Access denied'
+    })
+}
+
+let adminRole = (req, res, next) => {
     let role = req.query.role
-    if(role==='ADMIN') next();
+    if (role == 'ADMIN') next();
+    return res.status(422).json({
+        succes: false,
+        error: 'Access denied'
+    })
 }
 module.exports = {
-    verifyRole
+    create,
+    update,
+    adminRole
 }
